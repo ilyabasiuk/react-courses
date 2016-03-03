a0f8f5c0b8b777e13731864f45af0c7dcb2d6e66
Ilya Basiuk <ilyabasiuk@gmail.com>
2016-03-03 20:48:40 +0300

    added commentStore and apply it for comments

---
+++ lesson3/src/stores/CommentStore.js
@@ -0,0 +1,11 @@
+import SimpleStore from './SimpleStore'
+import AppDispatcher from '../dispatcher'
+
+class CommentStore extends SimpleStore {
+    constructor(...args) {
+        super(...args)
+
+    }
+}
+
+export default CommentStore
