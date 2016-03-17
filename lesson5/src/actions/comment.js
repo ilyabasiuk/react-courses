971f1901c140f6681a02668ce003fe17c28d3b81
Ilya Basiuk <ilyabasiuk@gmail.com>
2016-03-17 18:43:44 +0300

    adding loading Comments

--- lesson5/src/actions/comment.js
+++ lesson5/src/actions/comment.js
@@ -1,6 +1,6 @@
 import AppDispatcher from '../dispatcher'
-import { ADD_COMMENT, LOAD_COMMENTS_FOR_ARTICLE } from './constants'
-import { loadForArticle } from './api/comment'
+import { ADD_COMMENT, LOAD_COMMENTS_FOR_ARTICLE, LOAD_COMMENTS_BY_PARAMS } from './constants'
+import { loadForArticle, loadByParams } from './api/comment'
 import { asyncAC } from './api/utils'
 
 export function addComment(text, articleId) {
@@ -15,3 +15,4 @@ export function addComment(text, articleId) {
 }
 
 export const loadCommentsForArticle = asyncAC(LOAD_COMMENTS_FOR_ARTICLE, loadForArticle)
+export const loadCommentsByParams = asyncAC(LOAD_COMMENTS_BY_PARAMS, loadByParams)
--- lesson5/src/components/CommentsPage.js
+++ lesson5/src/components/CommentsPage.js
@@ -1,12 +1,32 @@
 import React, {Component, PropTypes} from "react"
-import commentStore from '../stores'
+import {commentStore} from '../stores'
+import {loadCommentsByParams as loadByParams} from '../actions/comment'
 
 class CommentsPage extends Component {
+    constructor(props) {
+        super()
+        this.commentsOnPage = 10
+        this.state = {
+            comments: commentStore.getAll()
+        }
+        loadByParams(this.getQueryParamsByPageNo(props.params.pageNo))
+    }
+
+    componentWillReceiveProps() {
+        console.log("wiil receive")
+    }
 
     render() {
         return (
             <h1>Comments Page {this.props.params.pageNo}</h1>
         )
+    }
+
+    getQueryParamsByPageNo (pageNo) {
+        return {
+            limit: this.commentsOnPage,
+            offset: this.commentsOnPage * (pageNo -1)
+        }
     }
 }
 
